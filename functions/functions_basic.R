cacad721b7f79492df7eecd7b429115056d8b861
Daniel S. Hain <38432063+daniel-hain@users.noreply.github.com>
2020-08-14 13:19:12 +0200

    desc update

--- functions/functions_basic.R
+++ functions/functions_basic.R
@@ -74,3 +74,18 @@ topicmodels_json_ldavis <- function(fitted, doc_dtm, method = "PCA", doc_in = NU
                                  reorder.topics = FALSE, mds.method = mds,plot.opts = list(xlab = "Dim.1", ylab = "Dim.2")) 
   return(json_lda)
 }
+
+##################################################################
+##	BEGIN: Jaccard Weight
+##################################################################
+
+weight_jaccard <- function(x, i, j, w) {
+  y <- x %>% select_(i, j, w)
+  colnames(y) <- c("i", "j", "w")
+  
+  y %<>% group_by(i) %>% mutate(w.i = sum(w)) %>% ungroup() %>%
+    group_by(j) %>% mutate(w.j = sum(w)) %>% ungroup() %>%
+    mutate(weight_jac = w / (w.i + w.j - w)) %>% 
+    select(weight_jac) %>% pull()
+  return(y) # TODO: Find a solution for the case when you have adittional groupings, eg. year
+}  
